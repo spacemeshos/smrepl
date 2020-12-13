517e43b0b74283d31a0d1a7b71e6a286295fc11b
Dave Appleton <calistralabs@gmail.com>
2020-12-13 14:23:56 +0800

    option to load or create a wallet

--- main.go
+++ main.go
@@ -26,13 +26,16 @@ func (m mockClient) Transfer(from, to, amount, passphrase string) error {
 }
 
 func main() {
+	walletPath := ""
 	dataDir := getwd()
+	var be *client.WalletBackend
+
 	grpcServer := client.DefaultGRPCServer
 	secureConnection := client.DefaultSecureConnection
 
 	flag.StringVar(&grpcServer, "server", grpcServer, fmt.Sprintf("The Spacemesh api grpc server host and port. Defaults to %s", client.DefaultGRPCServer))
 	flag.BoolVar(&secureConnection, "secure", secureConnection, "Connect securely to the server. Default is false")
-
+	flag.StringVar(&walletPath, "wallet", "", "path to existing wallet (default empty)")
 	flag.Parse()
 
 	_, err := syscall.Open("/dev/tty", syscall.O_RDONLY, 0)
@@ -40,7 +43,14 @@ func main() {
 		println(err)
 		os.Exit(1)
 	}
-	be, err := client.NewWalletBackend(dataDir, grpcServer, secureConnection)
+
+	if len(walletPath) > 0 {
+		fmt.Println("opening ", walletPath)
+		be, err = client.OpenWalletBackend(walletPath, grpcServer, secureConnection)
+	} else {
+		fmt.Println("creating wallet")
+		be, err = client.NewWalletBackend(dataDir, grpcServer, secureConnection)
+	}
 	if err != nil {
 		os.Exit(1)
 	}
