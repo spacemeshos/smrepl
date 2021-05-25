a3a491653d27b1cb63ef9555b3e0703898a0295a
avive <aviveyal07@gmail.com>
2021-05-25 17:14:07 +0300

    remove windoz crashing syscall check on startup

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/spacemeshos/smrepl/client"
-	"github.com/spacemeshos/smrepl/log"
-	"github.com/spacemeshos/smrepl/repl"
 	"os"
+
+	"github.com/spacemeshos/CLIWallet/client"
+	"github.com/spacemeshos/CLIWallet/repl"
 )
 
 func main() {
@@ -21,14 +21,13 @@ func main() {
 
 	flag.StringVar(&grpcServer, "server", grpcServer, fmt.Sprintf("The Spacemesh api grpc server host and port. Defaults to %s", client.DefaultGRPCServer))
 	flag.BoolVar(&secureConnection, "secure", secureConnection, "Connect securely to the server. Default is false")
-	flag.StringVar(&dataDir, "wallet_directory", getwd(), "set default wallet files directory")
-	flag.StringVar(&walletName, "wallet", "", "set the name of wallet file to open")
+	flag.StringVar(&dataDir, "wallet_directory", getwd(), "set default wallet directory")
+	flag.StringVar(&walletName, "wallet", "", "set the name of wallet to open")
 
 	flag.Parse()
 
 	be, err := client.OpenConnection(grpcServer, secureConnection, dataDir)
 	if err != nil {
-		flag.Usage()
 		os.Exit(1)
 	}
 	if walletName != "" {
@@ -36,17 +35,9 @@ func main() {
 		fmt.Println("opening ", walletPath)
 		be, err = client.OpenWalletBackend(walletPath, grpcServer, secureConnection)
 		if err != nil {
-			fmt.Println("failed to open wallet file : ", err)
+			fmt.Println("failed to open wallet : ", err)
 			os.Exit(1)
 		}
-	}
-
-	_, err = be.GetMeshInfo()
-	if err != nil {
-		log.Error("Failed to connect to mesh service at %v: %v", be.ServerInfo(), err)
-		fmt.Println()
-		flag.Usage()
-		os.Exit(1)
 	}
 
 	repl.Start(be)
